4342fcaa0371756148289f90d5848c63eaa864f1
David Agbesinyale <davidm7040@gmail.com>
2023-12-11 10:58:45 -0800

    corrections

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* DYNAMIC_LIB */
+#endif /* _MAIN_H */
